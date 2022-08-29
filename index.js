cf47e358fa27fda9ca3ba0b9e9a2e91aa71e96aa
Krish Tiwary <krish.tiwary@omneelab.com>
2022-08-29 21:38:22 +0530

    Node Js step to step

---
+++ index.js
@@ -0,0 +1,33 @@
+/*Variable declaration*/
+var x = 10;
+var y = 20;
+var c = x +y;
+console.warn(c);
+
+/*For loop */
+console.log('for loop ..');
+for(var i = 0; i < 10; i++)
+{
+   console.log(i);
+}
+
+/*while loop */
+console.log('while loop ..');
+var k = 0;
+while(k < 10)
+{
+    console.log(k);
+    k++;
+}
+/*do..while loop */
+console.log('do..while loop ..');
+let l = 0;
+do
+{
+    console.log(l);
+    l++;
+}
+while(l < 10)
+{
+    
+}
